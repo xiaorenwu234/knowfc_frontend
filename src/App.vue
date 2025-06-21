d378b72c6abf281e4ab606208487ed0df53053cc
xueht26 <13069167198@163.com>
2025-06-22 00:21:53 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button
+    class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+  >
+    Button
+  </button>
+</template>
+
+<style scoped></style>
