6a6b7f6d03386c593da261ff2e58e917cbfb399d
PhantomPower82 <phantompower82@hotmail.com>
2025-06-22 22:04:49 +0800

    edit tsconfig

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,32 @@
 <script setup lang="ts">
-import Dock from './components/Dock.vue';
-import { ref } from 'vue';
-const hideOthers = ref(false);
-const isNormal = ref(true);
-const isLogin = ref(false);
-const isCreatorView = ref(false);
+import DockBar from './components/DockBar.vue'
+import { ref } from 'vue'
+const hideOthers = ref(false)
+const isNormal = ref(true)
+const isLogin = ref(false)
+const isCreatorView = ref(false)
+const logo = document.getElementById('logo')!
 
 function leaveHome() {
-  document.getElementById('logo').classList.add('-mt-[84px]')
-  document.getElementById('logo').classList.remove('mt-60')
+  logo.classList.add('-mt-[84px]')
+  logo.classList.remove('mt-60')
   setTimeout(() => {
-    const scrollEvent = new Event('scroll');
-    window.dispatchEvent(scrollEvent);
-  }, 400)
-}
-
-function backHome() {
-  document.getElementById('logo').classList.remove('-mt-[84px]')
-  document.getElementById('logo').classList.add('mt-60')
-  isCreatorView.value=false
-  setTimeout(() => {
-    const scrollEvent = new Event('scroll');
-    window.dispatchEvent(scrollEvent);
+    const scrollEvent = new Event('scroll')
+    window.dispatchEvent(scrollEvent)
   }, 400)
 }
 </script>
 
 <template>
-  <div v-if="!hideOthers" class="fixed left-0 w-[100px] h-[100vh] flex flex-col justify-center z-50">
-
-    <Dock @leaveHome="leaveHome" :show-dock="isNormal"></Dock>
+  <div
+    v-if="!hideOthers"
+    class="fixed left-0 w-[100px] h-[100vh] flex flex-col justify-center z-50"
+  >
+    <DockBar @leaveHome="leaveHome" :show-dock="isNormal" />
   </div>
   <div class="flex flex-col">
     <div class="w-full h-screen z-0 flex justify-center items-center">
-      <RouterView></RouterView>
+      <RouterView />
     </div>
   </div>
 </template>
