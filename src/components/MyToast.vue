b6bf516120092398ad21aedcbdf3c83d5c315e47
PhantomPower82 <phantompower82@hotmail.com>
2025-06-25 14:52:36 +0800

    add field animation; refactor toast

---
+++ src/components/MyToast.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-lg font-bold">{{ title }}</div>
+  <div>{{ message }}</div>
+</template>
+
+<script setup lang="ts">
+const { title, message } = defineProps<{
+  title: string
+  message: string
+}>()
+</script>
