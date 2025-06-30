d6aedf369fe8da64daface0a49e2515bea6ecba7
wangzhenqi <wangzhenqi@kuaishou.com>
2025-06-30 14:00:54 +0800

    personal works display

--- src/components/PersonalCenterWorkDIsplay.vue
+++ src/components/PersonalCenterWorkDIsplay.vue
@@ -7,7 +7,8 @@
       v-model="selectedType"
       class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
     >
-      <option v-for="type in availableTypes" :key="type" :value="type">{{ translateType(type) }}</option>
+      <option v-for="type in availableTypes" :key="type" :value="type">{{ translateType(type) }}
+      </option>
     </select>
   </div>
 
@@ -83,7 +84,7 @@ const translateType = (type: string) => {
   const translations: { [key: string]: string } = {
     arxiv: 'arxiv',
     paper: '论文',
-    data: '数据',
+    data: '数据'
   }
   return translations[type] || type
 }
